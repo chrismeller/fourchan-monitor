4fa0db52ae60ddd2f9c4601f99ce4f3720f8f72d
Chris Meller <chris@doesnthaveone.com>
2021-12-22 11:37:23 +0200

    Fix the parsing of error logs.

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Transport } from '@nestjs/microservices';
 import { ConfigService } from '@nestjs/config';
-import { LogLevel } from '@nestjs/common';
+import { Logger, LogLevel } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     const configSerivce = app.get(ConfigService);
 
-    const logLevels = configSerivce.get<LogLevel[]>('LOG_LEVELS', [
-        'error',
-        'warn',
-    ]);
-    app.useLogger(logLevels);
+    const logLevels = configSerivce.get<string>('LOG_LEVELS', 'error,warn');
+    app.useLogger(logLevels.split(',') as LogLevel[]);
 
     app.connectMicroservice({
         transport: Transport.NATS,
